e76ae545db8176583e3d0f2697bc76443fe5d771
mobijalex <mobijalex6@gmail.com>
2023-06-18 22:27:30 +0100

    footer bug

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 // change navbar style in scroll
 
-window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle
-    ('window-scroll', window.scrollY > 0)
-})
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0);
+});
+
+// show hidden faq answers
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+
+    // change the icon
+    const icon = faq.querySelector(".faq_icon i");
+    if (icon.className === "uil uil-plus") {
+      icon.className = "uil uil-minus";
+    } else {
+      icon.className = "uil uil-plus";
+    }
+  });
+});
